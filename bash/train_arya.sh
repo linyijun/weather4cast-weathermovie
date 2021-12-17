b02544511baee2d131331256da55c6dd5b4bd53d
Yijun Lin <linyijun@users.noreply.github.com>
2021-12-17 17:03:48 -0600

    Update train_arya.sh

--- bash/train_arya.sh
+++ bash/train_arya.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -l
 
-cd /data/yijun/WeatherMovie/weather4cast-baselines/
 
-python train.py --data_path /data/yijun/WeatherMovie/preprocess-data/ --batch_size 4 --kernel_size 5 --h_dim 16 --seq_len 4 --horizon 4 --source_var_idx 0125 --target_var_idx 0 --model_name convttrans --num_test 10 --gpu_id 1
+python train.py --data_path /data/yijun/WeatherMovie/preprocess-data/ --batch_size 4 --kernel_size 5 --h_dim 32 --seq_len 4 --horizon 4 --source_var_idx 0125 --target_var_idx 0 --model_name convttrans --num_test 10 --gpu_id 1
 
 python test.py --data_path /data/yijun/WeatherMovie/preprocess-data/ --batch_size 4 --kernel_size 3 --h_dim 32 --seq_len 4 --horizon 4 --source_var_idx 0125 --target_var_idx 0 --model_name convttrans_seq4_hoz4_in0125_out0_kernel3_hdim32_1639348116 --num_test 100 --gpu_id 1
